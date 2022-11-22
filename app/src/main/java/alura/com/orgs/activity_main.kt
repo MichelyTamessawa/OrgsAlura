4ad311bbebce7c80963715c53c57fc087fc4d185
MichelyTamessawa <michelytamessawa@gmail.com>
2022-11-22 13:37:05 -0300

    Initial commit

---
+++ app/src/main/java/alura/com/orgs/activity_main.kt
@@ -0,0 +1,12 @@
+package alura.com.orgs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
